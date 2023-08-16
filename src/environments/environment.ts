c4423c21b4dfb925f845ee857cf2ef32647bb24a
TomkaYurii <tomka.yurii@gmail.com>
2023-08-16 18:41:49 +0300

    1st

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAdlPaeKXKEIXHHYeVG0bIRNeOC21CX1ZA",
+        authDomain: "kanbanfire-68e1a.firebaseapp.com",
+        databaseURL: "https://kanbanfire-68e1a-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "kanbanfire-68e1a",
+        storageBucket: "kanbanfire-68e1a.appspot.com",
+        messagingSenderId: "1033271346211",
+        appId: "1:1033271346211:web:b7871e9309f1f587bf1d96",
+        measurementId: "G-K858R5NT03"
+    },
+};
+
